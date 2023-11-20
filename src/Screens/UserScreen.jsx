63fdef96cbcd4c72a2334f231a19be4ebac88711
Joao Paulo Carvalho <joaopaullogcarvalho@gmail.com>
2023-11-20 17:02:25 -0300

    Added city and state fields to the user form.

--- src/Screens/UserScreen.jsx
+++ src/Screens/UserScreen.jsx
@@ -1,4 +1,4 @@
-
+import '../assets/css/UserScreen.css'
 import React, { useState } from 'react';
 
 function UserForm() {
@@ -7,6 +7,8 @@ function UserForm() {
         cpf: '',
         birthdate: '',
         email: '',
+        cidade: '',
+        estado: ''
     });
 
     const handleSubmit = (event) => {
@@ -20,32 +22,45 @@ function UserForm() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Nome:
-                <input type="text" name="name" value={user.name} onChange={handleChange} />
-            </label>
-            <label>
-                CPF:
-                <input type="text" name="cpf" value={user.cpf} onChange={handleChange} />
-            </label>
-            <label>
-                Data de Nascimento:
-                <input type="date" name="birthdate" value={user.birthdate} onChange={handleChange} />
-            </label>
-            <label>
-                Email:
-                <input type="email" name="email" value={user.email} onChange={handleChange} />
-            </label>
-            <button type="submit">Salvar</button>
+        <div className='box'>
+            <form onSubmit={handleSubmit}>
+                    <legend>Informações do usuário</legend>
+                    <br />
+                    <div className="inputBox">
+                        <input className='inputUser' type="text" name="name" value={user.name} onChange={handleChange} required />
+                        <label htmlFor='name' className='labelInput'>Nome Completo:</label>
+                    </div>
+                    
+                    <div className="inputBox">
+                        <input className='inputUser' type="text" name="cpf" value={user.cpf} onChange={handleChange} required/>
+                        <label  htmlFor='cpf' className='labelInput'>CPF:</label>
+                    </div>
+                    <div className="inputBox">
+                        <input className='inputUser' type="email" name="email" value={user.email} onChange={handleChange} required/>
+                        <label  htmlFor='email' className='labelInput'>Email:</label>
+                    </div>
+                    <br />
+                        <label htmlFor='birthdate'><b>Data de Nascimento:</b></label>
+                        <input type="date" name="birthdate" id='birthdate' value={user.birthdate} onChange={handleChange} required />
+                    <br /><br />
+                    <div className="inputBox">
+                        <input className='inputUser' type="text" name='cidade' value={user.cidade} onChange={handleChange} required/>
+                        <label htmlFor='cidade' className='labelInput'>Cidade</label>
+                    </div>
+                    <div className="inputBox">
+                        <input className='inputUser' type="text" name='estado' value={user.estado} onChange={handleChange} required/>
+                        <label htmlFor='estado' className='labelInput'>Estado</label>
+                    </div>
+                    
+                    <button id='submit' type="submit">Salvar</button>
         </form>
+        </div>
     );
 }
 
 function UserScreen() {
     return (
         <div>
-            <h1>Editar informações do usuário</h1>
             <UserForm />
         </div>
     );
